fcb0f000903bf380e0b2cf19b71900b833b8de7a
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-04 18:06:01 +0200

    a

---
+++ js/scroll.js
@@ -0,0 +1,34 @@
+
+var check = true
+function movetop() {
+    var body = $("html, body");
+    if ($(window).scrollTop() < $(window).height() * arguments[0]) {
+        body.stop().animate({scrollTop:$(window).height() * (arguments[0]-1)}, 100, 'swing', function() { 
+        });
+    } 
+    else {
+        body.stop().animate({scrollTop:$(window).height() * (arguments[0] + 1)}, 200, 'swing', function() { 
+        });
+    };
+};
+var v
+var lstpg
+$(window).scroll(function(){
+    if (theta % 1 == 0) {
+        clearTimeout(v)
+        window.check = false
+        window.lstpg = theta
+    }
+    else {
+        clearTimeout(v);
+        v = setTimeout(function() {
+            movetop(lstpg);
+        }, 500);
+    }
+    
+});
+
+$(window).addEventListener("touchend", function() {
+    console.log("hey")
+    movetop(lstpg);
+});
