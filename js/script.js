2904c7e91fbb57bc1f13e08c5bc3a635148346ff
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-04 18:29:30 +0200

    a

--- js/script.js
+++ js/script.js
@@ -35,6 +35,9 @@ $(window).resize(function() {
             height: 90 + "vw"
         });
     }
+    $(".second-page").css({
+        height: 5 * $(screen).height()
+    });
 })
 
 function reload_fix() {
@@ -336,32 +339,4 @@ function pop_down() {
     });
 }
 
-var check = true
-function movetop() {
-    var body = $("html, body");
-    if ($(window).scrollTop() < $(window).height() * arguments[0]) {
-        body.stop().animate({scrollTop:$(window).height() * (arguments[0]-1)}, 100, 'swing', function() { 
-        });
-    } 
-    else {
-        body.stop().animate({scrollTop:$(window).height() * (arguments[0] + 1)}, 200, 'swing', function() { 
-        });
-    };
-};
-var v
-var lstpg
-$(window).scroll(function(){
-    if (theta % 1 == 0) {
-        clearTimeout(v)
-        window.check = false
-        window.lstpg = theta
-    }
-    else {
-        clearTimeout(v);
-        v = setTimeout(function() {
-            movetop(lstpg);
-        }, 500);
-    }
-    
-});
 
