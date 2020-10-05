bb4b8bcdba75d5fdfd3337250a7fa2a0743fab63
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-05 17:15:58 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,27 +1,10 @@
-const staticCacheName = 'site-static-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/assets/js/ui.js',
-  '/assets/css/main.css',
-  '/assets/images/background-home.jpg',
-  'https://fonts.googleapis.com/css?family=Lato:300,400,700',
-];
-// install event
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
+const dynamicCacheName = 'site-dynamic-v1';
 // activate event
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key =>  key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
@@ -31,7 +14,12 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
     })
   );
 });
