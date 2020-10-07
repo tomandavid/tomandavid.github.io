a64837e772b84acbcaa8907c602be2884bed4505
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-07 15:27:20 +0200

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,13 @@
+var max_scroll_ios_fix = 5
+var check = true
+window.scroll_timeout = true
+window.v = 0
+var ios_fix
+window.lstpg = window.theta
+
+window.addEventListener("mousewheel", scroll_direction, false);
+window.addEventListener("DOMMouseScroll", scroll_direction, false);
+
 $(document).ready( function() {
     window.addEventListener( 'touchstart', function() {
         window.touchstart_scroll = $(window).scrollTop()
@@ -11,11 +21,40 @@ $(document).ready( function() {
         }
     });
 });
-var max_scroll_ios_fix = 5
-var check = true
+
+
+$(window).scroll(function(){
+    if (window.scroll_down) {
+        window.nxtpg = Math.ceil(theta)
+    }
+    else {
+        window.nxtpg = Math.floor(theta)
+    }
+        if (theta % 1 == 0) {
+            clearTimeout(window.v)
+            window.check = false
+            window.lstpg = theta
+        }
+        else {
+            if (window.device == "TouchPad") {
+                touchpad_scroll(500)
+            }
+            else if (window.device == "Mouse") {
+                touchpad_scroll(100)
+            }
+        }
+        if ($(window).width() < 1000) {
+            if (theta < 0.5 && theta != 0 && ios_fix == null) {
+                ios_fix_settimeout()
+            }
+            else {
+                ios_fix_cleartimeout()
+            }
+        }
+    });
+
 function movetop() {
     var body = $("html, body");
-    console.log("tady: "+window.innerHeight * arguments[0])
     body.stop().animate({scrollTop:$(window).height() * arguments[0]}, arguments[1], 'swing', function() {});
 };
 
@@ -31,63 +70,6 @@ function scroll_direction(e) {
         window.scroll_down = false
     }
 }
-window.addEventListener("mousewheel", scroll_direction, false);
-window.addEventListener("DOMMouseScroll", scroll_direction, false);
-
-window.mousescroll_run = true
-function mouse_scrolla() {
-    if (window.mousescroll_run) {
-        window.mousescroll_run = false
-        movetop(theta + arguments[0], 500)
-        console.log("a")
-    }
-}
-
-window.scroll_timeout = true
-window.v = 0
-var ios_fix
-window.lstpg = window.theta
-$(window).scroll(function(){
-if (window.scroll_down) {
-    window.nxtpg = Math.ceil(theta)
-}
-else {
-    window.nxtpg = Math.floor(theta)
-}
-    if (theta % 1 == 0) {
-        clearTimeout(window.v)
-        window.check = false
-        window.lstpg = theta
-    }
-    else {
-        if (window.device == "TouchPad") {
-            touchpad_scroll(500)
-        }
-        else if (window.device == "Mouse") {
-            touchpad_scroll(100)
-        }
-    }
-    if ($(window).width() < 1000) {
-        if (theta < 0.5 && theta != 0 && ios_fix == null) {
-            ios_fix_settimeout()
-        }
-        else {
-            ios_fix_cleartimeout()
-        }
-    }
-});
-
-
-
-
-
-
-
-
-
-
-var myVar;
-
 
 function ios_fix_settimeout() {
     ios_fix = setTimeout(function(){
@@ -99,7 +81,7 @@ function ios_fix_cleartimeout() {
     clearTimeout(ios_fix)
     ios_fix = null
 }
-window.scroll_timeout = true
+
 function mouse_scrolla() {
     console.log("nx"+nxtpg)
     if (window.scroll_timeout) {
@@ -117,51 +99,4 @@ function touchpad_scroll() {
    window.v = setTimeout(function() {
         movetop(nxtpg, 200 + "ms", 0);
     }, arguments[0]);
-}
-
-
-
-
-
-
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-function preventDefault(e) {
-  e.preventDefault();
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-
-// call this to Disable
-function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
