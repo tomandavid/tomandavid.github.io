6a3e22e9b7d86f1352b1c46f0795b3c216122575
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-09 11:38:25 +0200

    Update iexplorer.js

--- js/iexplorer.js
+++ js/iexplorer.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
     var ua = window.navigator.userAgent;
     var msie = ua.indexOf('MSIE ');
     if (msie > 0) {
-    // IE 10 or older => return version number
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-        alert("ahoj")
+        alert("Unfortunately your browser is not supported.")
     }
 });
