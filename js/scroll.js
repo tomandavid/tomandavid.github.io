ccdc9976c667892022525b56f8b620dc310316f2
tomandavid <65895415+tomandavid@users.noreply.github.com>
2020-10-19 12:30:49 +0200

    a

--- js/scroll.js
+++ js/scroll.js
@@ -33,15 +33,6 @@ $(window).scroll(function(){
     fix_settimeout()
     dot_slider()
 
-    if ($(window).width() < 1000) {
-        if (theta < 0.5 && theta != 0 && ios_fix == null) {
-            ios_fix_settimeout()
-        }
-        else {
-            ios_fix_cleartimeout()
-        }
-    }
-
     window.last_scrolltop = $(window).scrollTop()
 });
 
@@ -60,18 +51,6 @@ function fix_cleartimeout() {
     clearTimeout(ios_fix)
     ios_fix = null
 }
-
-function ios_fix_settimeout() {
-    ios_fix = setTimeout(function(){
-        movetop(0, 200 + "ms", 0)
-    }, 1000);
-}
-
-function ios_fix_cleartimeout() {
-    clearTimeout(ios_fix)
-    ios_fix = null
-}
-
 
 function dot_slider() {
 
